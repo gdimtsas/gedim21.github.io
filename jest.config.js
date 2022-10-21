4455b06cb06668f676a1b26adf1d895b483559fb
Georgios Dimtsas <gedim21@gmail.com>
2022-10-21 13:27:42 +0300

    Updated deps

--- jest.config.js
+++ jest.config.js
@@ -15,13 +15,10 @@ module.exports = {
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.ts`,
     '@/(.*)': '<rootDir>/src/$1',
   },
-  setupFiles: [`<rootDir>/__helpers__/loadershim.ts`],
-  setupFilesAfterEnv: ['<rootDir>/__helpers__/setup.ts'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.svg$': '<rootDir>/__mocks__/svgTransform.ts',
   },
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
 };
